ee36ec36c982b4c9973b9a3f5e7aa7cdd4ba87a4
Ave Jay Moradillo <Ave Jay Moradillo@LLI-PC-AVE>
2024-02-02 16:50:58 +0800

    My first commit with JavaCoreLearnings

---
+++ src/IncrementDecrementOperators/PreDecrementOperator.java
@@ -0,0 +1,8 @@
+package IncrementDecrementOperators;
+
+public class PreDecrementOperator {
+    public static void main(String[] args) {
+        int i = 4;
+        System.out.println(--i);
+    }
+}
