30da06c44e538d5fecdb72dd5ba5bf9e42d76eb3
Ave Jay Moradillo <Ave Jay Moradillo@LLI-PC-AVE>
2024-03-27 16:10:42 +0800

    Updated Array Looping

--- src/Experiment/Test5.java
+++ src/Experiment/Test5.java
@@ -3,9 +3,41 @@ package Experiment;
 public class Test5 {
     public static void main(String[] args) {
 
-        System.out.println("Test5");
-        System.out.println("Test5");
-        System.out.println("Test5");
-        System.out.println("Test5");
+        String[][] table = {
+                {"Agumon", "AA", "Candlemon", "DA", "Betamon", "IA"},
+                {"Biyomon", "AB", "Crabmon", "DB", "DemiDevimon", "IB"},
+                {"ClearAgumon", "AC", "Elecmon", "DC", "Dokunemon", "IC"},
+                {"Gomamon", "AD", "Floramon", "DD", "Gazimon", "ID"},
+                {"Penguinmon", "AE", "Gabumon", "DE", "Gizamon", "IE"},
+                {"SnowAgumon", "AF", "Gotsumon", "DF", "Goburimon", "IF"},
+                {"Tapirmon", "AG", "Palmon", "DG", "Hagurumon", "IG"},
+                {"Tentomon", "AH", "Patamon", "DH", "Kunemon", "IH"},
+                {"ToyAgumon", "AI", "", "Mushroomon", "II"},
+                {"Veemon", "AJ", "", "Otamamon", "IJ"},
+                {"", "", "", "SnowGoburimon", "IK"},
+                {"", "", "", "Syakomon", "IL"},
+                {"", "", "", "Tsukaimon", "IM"}
+        };
+
+        System.out.println("+-------------+----+-----------+----+---------------+----+");
+        for (int i = 0; i < table.length; i++)
+        {
+            System.out.print("| ");
+            for (int j = 0; j < table[i].length; j++)
+            {
+                if (!table[i][j].isEmpty())
+                {
+                    System.out.print(table[i][j]);
+                }
+                System.out.print(" | ");
+            }
+            System.out.println();
+            if (i < table.length - 1)
+            {
+                System.out.println("+-------------+----+-----------+----+---------------+----+");
+            }
+        }
+        System.out.println("+-------------+----+-----------+----+---------------+----+");
     }
 }
+
